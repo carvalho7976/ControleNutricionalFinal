ae42d7675852c8bcae4d092973f55c4677dda3de
ROGERIO CARVALHO <rogerio_carvalho15@hotmail.com>
2015-06-17 12:49:00 -0300

    relatorios teste

---
+++ ControleNutricionalFinal/Models/Relario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleNutricionalFinal.Models {
+    public class Relario {
+
+        public List<Refeicao> Refeicao;
+        public double Calorias { get; set; }
+        
+    }
+}
